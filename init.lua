948b8938c219a8f9d7951479c2c9d04a55ebbdd8
jacob <RegenJacob@gmx.de>
2023-08-09 19:02:47 +0200

    formatting

--- init.lua
+++ init.lua
@@ -2,9 +2,8 @@ vim.loader.enable()
 
 require("options")
 
+-- let neovim run even if plugins fail
 local ok, e = pcall(require, "core")
 if not ok then
   print("Error while loading Plugins! \n Error: " .. e)
 end
-
---require("lsp").set_capabilities()
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     end,
     dependencies = {
       'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons'     -- optional
+      'nvim-tree/nvim-web-devicons'      -- optional
     }
   },
   {
@@ -20,7 +20,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = true,
-    config = function ()
+    config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "rust_analyzer" },
       })
@@ -39,84 +39,92 @@ return {
   {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdlineEnter" },
-    config = function ()
-  local cmp = require("cmp")
+    config = function()
+      local cmp = require("cmp")
 
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-        if cmp.visible() then
-          local entry = cmp.get_selected_entry()
-          if not entry then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            cmp.confirm()
-          end
-        else
-          fallback()
-        end
-      end, { "i", "s", "c" }),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    exprimental = {
-      ghost_text = false,
-      native_menu = false,
-    },
-    sources = cmp.config.sources({
-      { name = "copilot",                group_index = 2 },
-      { name = "nvim_lsp" },
-      { name = "luasnip" }, -- For luasnip users.
-      { name = "nvim_lua" },
-      { name = "nvim_lsp_signature_help" },
-    }, {
-        { name = "buffer" },
-      }),
-  })
+      ---@diagnostic disable: missing-fields
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          --completion = cmp.config.window.bordered(opts),
+          documentation = {
+            border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' }
+          }
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+            if cmp.visible() then
+              local entry = cmp.get_selected_entry()
+              if not entry then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              else
+                cmp.confirm()
+              end
+            else
+              fallback()
+            end
+          end, { "i", "s", "c" }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        exprimental = {
+          ghost_text = false,
+          native_menu = false,
+        },
+        sources = cmp.config.sources({
+          { name = "copilot",                group_index = 2 },
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          { name = "nvim_lua" },
+          { name = "nvim_lsp_signature_help" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
 
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype("gitcommit", {
-    sources = cmp.config.sources({
-      { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-        { name = "buffer" },
-      }),
-  })
+      -- Set configuration for specific filetype.
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+        }, {
+          { name = "buffer" },
+        }),
+      })
 
-  vim.api.nvim_create_autocmd("BufRead", {
-    group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-    pattern = "Cargo.toml",
-    callback = function()
-      cmp.setup.buffer({ sources = { { name = "crates" } } })
-    end,
-  })
+      vim.api.nvim_create_autocmd("BufRead", {
+        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+        pattern = "Cargo.toml",
+        callback = function()
+          cmp.setup.buffer({ sources = { { name = "crates" } } })
+        end,
+      })
 
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ "/", "?" }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" },
-    },
-  })
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" },
-    }, {
-        { name = "cmdline" },
-      }),
-  })
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+      ---@diagnostic enable: missing-fields
     end,
     dependencies = {
       "saecki/crates.nvim",
